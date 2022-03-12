e4e352ff4b551236a8a02d80e83d34f852650358
sitinurkaffah <92791203+sitinurkaffah@users.noreply.github.com>
2022-03-12 19:48:45 +0700

    Praktikum Pemograman Web 2

---
+++ Praktikum 3/view/artikel2.php
@@ -0,0 +1,32 @@
+<?php
+include '../layout/header.php'
+?>
+
+<h1>Welcome To Website</h1>
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12">
+			<blockquote class="blockquote">
+				<p class="mb-0">
+                    Pendidikan bukan cuma pergi ke sekolah dan mendapatkan gelar. 
+                    Tapi juga soal memperluas pengetahuan dan menyerap ilmu kehidupan.
+				</p>
+				<footer class="blockquote-footer">
+					<cite>— Shakuntala Devi</cite>
+				</footer>
+			</blockquote>
+			<blockquote class="blockquote">
+				<p class="mb-0">
+                    Jangan menyerah. Menderitalah sekarang dan hiduplah sebagai juara nantinya.
+				</p>
+				<footer class="blockquote-footer">
+					<cite>— Muhammad Ali</cite>
+				</footer>
+			</blockquote>
+		</div>
+	</div>
+</div>
+
+<?php
+include '../layout/footer.php'
+?>
